d156057f534e65bab8dac032e8fdc938ba0a1795
kndavies <kdavies@wccnet.edu>
2015-07-29 16:04:21 -0400

    added countries.json

--- src/travelItems/Item.java
+++ src/travelItems/Item.java
@@ -5,12 +5,8 @@ public class Item {
 	String name;
 	int maxTemp;
 	int minTemp;
-	int Cloud;
 	int weatherCode;
-	int extreme;
-	boolean essentialItem;
 	public boolean include;
-	String warning;
 
 	public Item(String name, int minTemp, int maxTemp, int cloud, int weatherCode, int extreme, boolean essentialItem, boolean include, String warning) {
 		super();
@@ -18,20 +14,13 @@ public class Item {
 		this.maxTemp = maxTemp;
 		this.minTemp = minTemp;
 		this.weatherCode = weatherCode;
-		this.extreme = extreme;
-		this.essentialItem = essentialItem;
 		this.include = include;
-		this.warning = warning;
 	}
 
 
 	public void includes() {
-		
-		if (essentialItem == true){
-			include = true;
-			}
 		//this will account for items needed when raining
-		else if (WeatherMap.getWeatherCode() < 600 && WeatherMap.getWeatherCode() > 0 && weatherCode < 600 && weatherCode > 0 ){
+		if (WeatherMap.getWeatherCode() < 600 && WeatherMap.getWeatherCode() > 0 && weatherCode < 600 && weatherCode > 0 ){
 			include = true;
 		}
 		//this will account for items needed for sunny days
@@ -52,12 +41,10 @@ public class Item {
 		
 		//this will account for items needed for extreme hot days
 		else if (WeatherMap.getExtreme() == 904 && extreme == WeatherMap.getExtreme()){
-			warning = "Heat Advisory! Please pack accordingly";
 			include = true;}
 		
 		//this will account for items needed for extreme cold days
 		else if (WeatherMap.getExtreme() == 903 && extreme == WeatherMap.getExtreme()){
-			warning = "Cold Advisory! Please pack accordingly";
 			include = true;}
 		
 		//this will account for items needed for windy days
