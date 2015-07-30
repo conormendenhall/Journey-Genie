2d7cb658c0be3038a930bbcc104f08c7d4764957
kndavies <kdavies@wccnet.edu>
2015-07-30 14:56:14 -0400

    modified trip class

--- src/travelItems/Trip.java
+++ src/travelItems/Trip.java
@@ -12,7 +12,19 @@ public class Trip implements Serializable{
 	private static final long serialVersionUID = 618353573734930660L;
 	private WeatherInfoObject w;
 	private ArrayList<Item> items;
+	private PackingList packing = new PackingList();
 	private ArrayList<String> dates;
+	
+	public Trip()
+	{
+		items.add(packing.getConditioner());
+		items.add(packing.getDeodorant());
+		items.add(packing.getShampoo());
+		items.add(packing.getShaver());
+		items.add(packing.getSoap());
+	}
+	
+	
 	public WeatherInfoObject getW() {
 		return w;
 	}
--- test/travelItemsTest/ProjPracTest.java
+++ test/travelItemsTest/ProjPracTest.java
@@ -39,53 +39,54 @@ public class ProjPracTest {
 		p.getScarf().includes(802, 20, 40);
 		assertEquals(true, p.getScarf().include);
 	}	
-	
-	@Test
-	public void shouldReturnTrueForNonEssentialItemNeededForHotTemperatureIsTrue () {
-		PackingList p = new PackingList();
-		p.getSunGlasses().includes(802, 70, 80);
-		assertEquals(true, p.getSunGlasses().include);
-	}	
-	
-	@Test
-	public void shouldReturnTrueForNonEssentialItemNeededForSnowCodes () {
-		PackingList p = new PackingList();
-		p.getSnowBoots().includes(602, 70, 80);
-		assertEquals(true, p.getSnowBoots().include);
-	}	
-	
-	
-	
-	/*@Test
-	public void shouldReturnTrueForNonEssentialItemNeededForExtremeHotTemperatureIsTrue () {
-		Item sunGlasses = new Item("sunGlasses", 0, 0, 904, false);
-		WeatherMap.setExtreme(904);
-		sunGlasses.includes();
-		assertEquals(true, sunGlasses.include);
-	}
-	
-	@Test
-	public void shouldReturnTrueForNonEssentialItemNeededForExtremeColdTemperatureIsTrue () {
-		Item longUndies = new Item("longUndies", 0, 0, 903, false);
-		WeatherMap.setExtreme(903);
-		longUndies.includes();
-		assertEquals(true, longUndies.include);
-	}
-	
-	
-	
-	/*@Test
-	public void shouldReturnTrueForLongUnderwearWhenTempIs32Degrees() {
-		Item longUnderwear = new Item(-99, 55, false, false, false);
-		WeatherInfo.setTemp(66);
-		assertEquals(true, Item.include(longUnderwear));
-	}
-	
-	@Test
-	public void shouldReturnTrueForWaterWhenTempIs100Degrees() {
-		Item water = new Item(75, 120, false, true, false);
-		WeatherInfo.setSunny(false);
-		assertEquals(true, Item.include(water));
-	}*/
+//	
+//	@Test
+//	public void shouldReturnTrueForNonEssentialItemNeededForHotTemperatureIsTrue () {
+//		Item sunGlasses = new Item("sun Glasses", 80, 0, 0, false);
+//		WeatherMap.setTemp(85);
+//		sunGlasses.includes();
+//		assertEquals(true, sunGlasses.include);
+//	}
+//	
+//	@Test
+//	public void shouldReturnTrueForNonEssentialItemNeededForSnowCodes () {
+//		Item snowBoots = new Item("snow boots", 0, 0, 601, false);
+//		WeatherMap.setWeatherCode(602);
+//		snowBoots.includes();
+//		assertEquals(true, snowBoots.include);
+//
+//	}	
+//	
+//	@Test
+//	public void shouldReturnTrueForNonEssentialItemNeededForExtremeHotTemperatureIsTrue () {
+//		Item sunGlasses = new Item("sunGlasses", 0, 0, 904, false);
+//		WeatherMap.setExtreme(904);
+//		sunGlasses.includes();
+//		assertEquals(true, sunGlasses.include);
+//	}
+//	
+//	@Test
+//	public void shouldReturnTrueForNonEssentialItemNeededForExtremeColdTemperatureIsTrue () {
+//		Item longUndies = new Item("longUndies", 0, 0, 903, false);
+//		WeatherMap.setExtreme(903);
+//		longUndies.includes();
+//		assertEquals(true, longUndies.include);
+//	}
+//	
+//	
+//	
+//	/*@Test
+//	public void shouldReturnTrueForLongUnderwearWhenTempIs32Degrees() {
+//		Item longUnderwear = new Item(-99, 55, false, false, false);
+//		WeatherInfo.setTemp(66);
+//		assertEquals(true, Item.include(longUnderwear));
+//	}
+//	
+//	@Test
+//	public void shouldReturnTrueForWaterWhenTempIs100Degrees() {
+//		Item water = new Item(75, 120, false, true, false);
+//		WeatherInfo.setSunny(false);
+//		assertEquals(true, Item.include(water));
+//	}
 	
 }
